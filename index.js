c72fd5e14498b4fef67ef9dae2350b728a9e66fc
Linuxgeekforgeeks <mahmousalisaaa@gmail.com>
2023-07-16 20:51:49 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const app=express()
+
+const db=require("./models")
+  
+db.sequelize.sync().then(()=>{
+app.listen(3001,()=>{
+    console.log("The server Has Started on Port 3001")
+})
+})
+
+
+
+
+
+
+
